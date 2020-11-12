20dbd2bc0a4613a5e56de4f66610168fb09ba90a
emzilla91 <63753984+emzilla91@users.noreply.github.com>
2020-11-12 09:50:27 +0100

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public LevelsBuilder m_LevelsBuilder;
+    public GameObject m_ResetButton;
+    private bool m_ReadyForInput;
+    private Player m_Player;
+    public int brLevela;
+
+    private Timer theTimer;
+
+    void Start()
+    {
+        theTimer = FindObjectOfType<Timer>();
+        m_ResetButton.SetActive(true);
+        ResetScene();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveInput.Normalize();
+        if (moveInput.sqrMagnitude > 0.5)
+        {
+            if (m_ReadyForInput)
+            {
+                m_ReadyForInput = false;
+                m_Player.Move(moveInput);
+                EndGame();
+            }
+        }
+        else
+        {
+            m_ReadyForInput = true;
+        }
+    }
+
+    public void NextLevel()
+    {
+        m_LevelsBuilder.NextLevel();
+        StartCoroutine(ResetSceneASync());
+        brLevela++;
+    }
+
+    public void ResetScene()
+    {
+        StartCoroutine(ResetSceneASync());
+    }
+
+    public void EndGame()
+    {
+        if (brLevela == 4 && IsLevelComplete())
+        {          
+            m_ResetButton.SetActive(false);
+            theTimer.scoreIncreasing = false;
+            theTimer.GameEnd();
+        }
+        else if (IsLevelComplete())
+        {
+            NextLevel();
+        }
+    }
+    bool IsLevelComplete()
+    {
+        Box[] boxes = FindObjectsOfType<Box>();
+        foreach (var box in boxes)
+        {
+            if (!box.m_OnCross) return false;
+        }
+        return true;
+    }
+
+    IEnumerator ResetSceneASync()
+    {
+        if (SceneManager.sceneCount > 1)
+        {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync("LevelScene");
+            while (!asyncUnload.isDone)
+            {                                                                                         
+                yield return null;
+                Debug.Log("UnLoading...");
+            }
+            Debug.Log("Unload Done");
+            Resources.UnloadUnusedAssets();
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelScene", LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            Debug.Log("Loading...");
+        }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("LevelScene"));
+        m_LevelsBuilder.Build();
+        m_Player = FindObjectOfType<Player>();
+        Debug.Log("Level loaded " + brLevela);
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public Text bestTime;
+    public Text newBest;
+
+    public float startTimeCount;
+    public float pointsPerSec;
+    public bool scoreIncreasing = true;
+
+    public static float bestTimeCount = -1;
+
+
+    void Start()
+    {
+        newBest.text = "";
+        ReadFromFile();
+
+        if (bestTimeCount != -1)
+            bestTime.text = "Best time: " + Mathf.Round(Timer.bestTimeCount);
+    }
+
+    void Update()
+    {
+
+        if (scoreIncreasing)
+        {
+            startTimeCount += pointsPerSec + Time.deltaTime;
+        }
+
+
+        timerText.text = "Time: " + Mathf.Round(startTimeCount);
+    }
+
+    public void GameEnd()
+    {
+
+        if ((startTimeCount < Timer.bestTimeCount) || (Timer.bestTimeCount == -1))
+        {
+            Timer.bestTimeCount = startTimeCount;
+            PlayerPrefs.SetFloat("BestTime", (Timer.bestTimeCount));
+
+            newBest.text = "!!New best time!!";
+        }
+        bestTime.text = "Best time: " + Mathf.Round(Timer.bestTimeCount);
+
+        StoreToFile();
+    }
+
+    private void StoreToFile()
+    {
+        string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+        string specificFolder = Path.Combine(folder, "Soko");
+        Directory.CreateDirectory(specificFolder);
+
+        File.WriteAllText(Path.Combine(specificFolder, "besttime"), Timer.bestTimeCount.ToString());
+
+    }
+
+    private void ReadFromFile()
+    {
+        string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+        string specificFolder = Path.Combine(folder, "Soko");
+
+        if (File.Exists(Path.Combine(specificFolder, "besttime")))
+        {
+            Timer.bestTimeCount = float.Parse(File.ReadAllText(Path.Combine(specificFolder, "besttime")));
+        }
+    }
+}
